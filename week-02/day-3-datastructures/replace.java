59237404bc0a96cbdd24ce9bfa1c13e66249f6a3
lilla <lilla@quince.hu>
2018-04-15 20:55:43 +0200

    [datastructures] replace fix

---
+++ week-02/day-3-datastructures/replace.java
@@ -0,0 +1,11 @@
+public class replace {
+
+    public static void main(String[] args){
+        String original = "In a dishwasher far far away";
+
+        String amend = original.replace("dishwasher", "galaxy");
+
+        System.out.println(amend);
+    }
+}
+
