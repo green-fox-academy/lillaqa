289334c86e13f423ce80bc8df11fe81f513bbbbf
lilla <lilla@quince.hu>
2018-04-21 23:26:27 +0200

    replace fixed

--- week-02/day-3-datastructures/Replace.java
+++ week-02/day-3-datastructures/Replace.java
@@ -1,11 +1,18 @@
+package DataStructures;
+
 public class Replace {
 
     public static void main(String[] args){
-        String original = "In a dishwasher far far away";
 
-        String amend = original.replace("dishwasher", "galaxy");
+        String amend = replace("In a dishwasher far far away","dishwasher","galaxy");
 
         System.out.println(amend);
+    }
+
+    static String replace(String text, String toChange, String amended){
+        text = text.replaceAll(toChange,amended);
+        return text;
+
     }
 }
 
