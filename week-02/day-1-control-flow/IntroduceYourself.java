d6695dd062d2629aedb1f5e32a5950635b354ed5
lilla <lilla@quince.hu>
2018-04-21 23:25:29 +0200

    add new task int. ys.

---
+++ week-02/day-1-control-flow/IntroduceYourself.java
@@ -0,0 +1,16 @@
+package ControlFlow;
+
+public class IntroduceYourself {
+
+    public static void main(String[] args){
+
+        String firstName = "Lilla";
+        int age = 32;
+        double height = 1.6;
+
+
+        System.out.println(firstName + "\n" + age + "\n" + height);
+
+
+    }
+}
