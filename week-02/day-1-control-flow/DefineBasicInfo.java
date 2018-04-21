add5506129cddbd818947e771d0767aa8d700551
lilla <QuiHUlil14!>
2018-04-21 19:55:09 +0200

    add finished tasks

---
+++ week-02/day-1-control-flow/DefineBasicInfo.java
@@ -0,0 +1,22 @@
+package ControlFlow;
+
+public class DefineBasicInfo {
+
+    public static void main(String[] args){
+
+        String firstName = "Lilla";
+        int age = 32;
+        double height = 1.6;
+        boolean isMarried = false;
+
+        System.out.println("My name is: " + firstName + "\nMy age is: " + age + "\nMy height is: " + height + "\nAm i married? " + isMarried);
+
+    }
+}
+
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
---
+++ week-02/day-1-control-flow/FavoriteNumber.java
@@ -0,0 +1,13 @@
+package ControlFlow;
+
+public class FavoriteNumber {
+
+    public static void main(String[] args){
+
+        int favoriteNumber = 5;
+
+        System.out.println("My favorite number is: " + favoriteNumber);
+
+    }
+}
+
