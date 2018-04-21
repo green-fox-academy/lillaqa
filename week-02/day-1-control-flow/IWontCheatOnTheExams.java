5f2ad96399c0a8dea4279ec52c036933aa5924db
lilla <lilla@quince.hu>
2018-04-22 00:01:38 +0200

    i won't cheat

---
+++ week-02/day-1-control-flow/IWontCheatOnTheExams.java
@@ -0,0 +1,20 @@
+package ControlFlow;
+
+public class IWontCheatOnTheExams {
+
+    public static void main(String[] args){
+
+        String punishment = "I won't cheat on the exam!";
+
+
+        for (int i=0; i<100; i++){
+            System.out.print(punishment + "\n");
+        }
+
+
+    }
+}
+
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
