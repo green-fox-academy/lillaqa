90c853f711e22ae98d43eb99b38b7f041c78702c
lilla <lilla@quince.hu>
2018-04-22 20:39:46 +0200

    secs in a day ready

---
+++ week-02/day-1-control-flow/SecondsInADay.java
@@ -0,0 +1,21 @@
+package ControlFlow;
+
+public class SecondsInADay{
+
+    public static void main(String[] args){
+
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        int allSec = 24*60*60;
+        int currentTime = currentSeconds + currentMinutes*60 + currentHours*3600;
+        int remainingSec = allSec - currentTime;
+        System.out.println(remainingSec + " second remaining from today! hurry up!");
+
+    }
+
+}
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by the variables
