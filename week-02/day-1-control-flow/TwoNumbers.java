f0127fad61603918db657a3ea37ee3bdcdb00eba
lilla <lilla@quince.hu>
2018-04-22 16:24:06 +0200

    twonumbers

---
+++ week-02/day-1-control-flow/TwoNumbers.java
@@ -0,0 +1,27 @@
+package ControlFlow;
+
+public class TwoNumbers {
+
+    public static void main(String[] args){
+        int number1 = 22;
+        int number2 = 13;
+        double number3 = 13;
+
+        System.out.println(number2 + number1);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number3);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
+
+    }
+}
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+// Print the result of 13 added to 22
+// Print the result of 13 substracted from 22
+// Print the result of 22 multiplied by 13
+// Print the result of 22 divided by 13 (as a decimal fraction)
+// Print the integer part of 22 divided by 13
+// Print the reminder of 22 divided by 13
+
