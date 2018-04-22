320733848ab4635e23b47de7fe84c6df5065dc95
lilla <lilla@quince.hu>
2018-04-22 15:28:14 +0200

    partyindicator

---
+++ week-02/day-1-control-flow/PartyIndicator.java
@@ -0,0 +1,48 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class PartyIndicator {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many girls are coming?");
+        int girls = scanner.nextInt();
+        System.out.println("How many boys are coming?");
+        int boys = scanner.nextInt();
+        scanner.close();
+
+        int allParticipant = boys + girls;
+
+
+        if((girls == boys) && (allParticipant > 20)){
+            System.out.println("The party is exellent!");
+        }
+        else if((girls != boys) && (allParticipant > 20)){
+            System.out.println("Quite cool party!");
+        }
+        else if(allParticipant < 20){
+            System.out.println("Average party...");
+        }
+        else if(girls == 0){
+            System.out.println("Sausage party");
+        }
+
+    }
+}
+// Write a program that asks for two numbers
+// Thw first number represents the number of girls that comes to a party, the
+// second the boys
+// It should print: The party is exellent!
+// If the the number of girls and boys are equal and there are more people coming than 20
+//
+// It should print: Quite cool party!
+// It there are more than 20 people coming but the girl - boy ratio is not 1-1
+//
+// It should print: Average party...
+// If there are less people coming than 20
+//
+// It should print: Sausage party
+// If no girls are coming, regardless the count of the people
