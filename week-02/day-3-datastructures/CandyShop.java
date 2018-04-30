e15cba74ae617417dd7140cd08ba93d3f23a2bae
lilla <lilla@quince.hu>
2018-04-30 11:30:43 +0200

    add candyshop

---
+++ week-02/day-3-datastructures/CandyShop.java
@@ -0,0 +1,23 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class CandyShop {
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add("Cupcake");
+        arrayList.add(2);
+        arrayList.add("Brownie");
+        arrayList.add(false);
+
+        System.out.println(sweets(arrayList));
+    }
+
+    public static ArrayList sweets(ArrayList<Object> arrayList) {
+        arrayList.set(1, "Croissant");
+        arrayList.set(3, "Ice cream");
+        return arrayList;
+    }
+}
