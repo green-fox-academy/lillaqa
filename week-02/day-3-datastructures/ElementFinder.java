c827295b6fdafea2fdb5f6af3b97bd784043a542
lilla <lilla@quince.hu>
2018-04-30 12:23:06 +0200

    element finder first solution

---
+++ week-02/day-3-datastructures/ElementFinder.java
@@ -0,0 +1,28 @@
+package DataStructures;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ElementFinder {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        System.out.println(containsSeven(arrayList));
+    }
+
+    public static String containsSeven(ArrayList arrayList) {
+
+        String result = "";
+        if (arrayList.contains(7)) {
+            result = "Hoorray";
+        }
+        else {
+            result = "Noooooo";
+        }
+
+        return result;
+    }
+}
+
