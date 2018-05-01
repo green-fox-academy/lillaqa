5f6969a8f1c084948351a1104a4f4d6ea66f110b
lilla <lilla@quince.hu>
2018-05-01 22:46:06 +0200

    doable task initiative commit, WIP

---
+++ week-05/src/day1ObjectOriented/Trees.java
@@ -0,0 +1,20 @@
+package day1ObjectOriented;
+
+public class Trees {
+
+    String type;
+    String leafColor;
+    int age;
+    boolean sex;
+
+    public static void main(String[] args) {
+        Trees oak = new Trees();
+        Trees birch = new Trees();
+        Trees cherry = new Trees();
+        Trees elm = new Trees();
+        Trees hickory = new Trees();
+    }
+
+    
+
+}
