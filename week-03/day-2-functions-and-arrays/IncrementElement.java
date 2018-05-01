acb978523addab2420a912360dcc182b93310715
lilla <lilla@quince.hu>
2018-05-01 21:17:25 +0200

    increment element ready

---
+++ week-03/day-2-functions-and-arrays/IncrementElement.java
@@ -0,0 +1,11 @@
+package ArraysAndFunctions;
+
+public class IncrementElement {
+
+    public static void main(String[] args) {
+
+        int[] t = {1, 2, 3, 4, 5};
+        t[2] = t[2] + 3;
+        System.out.println(t[2]);
+    }
+}
