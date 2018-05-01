53ea0ea005c3bd32966f4b930f6067dceba475dd
lilla <lilla@quince.hu>
2018-05-01 21:12:17 +0200

    small fix - array initialization

--- week-03/day-2-functions-and-arrays/CompareLenght.java
+++ week-03/day-2-functions-and-arrays/CompareLenght.java
@@ -4,8 +4,8 @@ public class CompareLenght {
 
     public static void main(String[] args) {
 
-        int p1[] = {1, 2, 3};
-        int p2[] = {4, 5};
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
 
         if(p1.length < p2.length) {
             System.out.println("P1 is a longer list");
