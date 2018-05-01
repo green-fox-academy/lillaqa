31b5f9889cf53bd276a374e1f791fc922084c1ca
lilla <lilla@quince.hu>
2018-05-01 21:04:36 +0200

    remove comments

--- week-03/day-2-functions-and-arrays/SumElements.java
+++ week-03/day-2-functions-and-arrays/SumElements.java
@@ -7,9 +7,7 @@ public class SumElements {
 
     }
 }
-// - Create an array variable named `r`
-//   with the following content: `[54, 23, 66, 12]`
-// - Print the sum of the second and the third element
+
 
 
 
