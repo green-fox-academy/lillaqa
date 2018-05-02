2054c427039c774926bdc3973c65e54863a48583
lilla <lilla@quince.hu>
2018-05-02 20:55:46 +0200

    blog post

--- week-05/src/day1ObjectOriented/preClassTask/day1ObjectOriented/blogPost/BlogPost.java
+++ week-05/src/day1ObjectOriented/preClassTask/day1ObjectOriented/blogPost/BlogPost.java
@@ -1,4 +1,13 @@
 package day1ObjectOriented.preClassTask.day1ObjectOriented.blogPost;
 
 public class BlogPost {
+
+    public static void main(String[] args) {
+        PostAttributes loremIpsum = new PostAttributes("Lorem Ipsum", "John Doe", "2000.05.04.", "Lorem ipsum dolor sit amet.");
+
+        PostAttributes waitButWhy = new PostAttributes("Wait but why", "Tim Urban", "2010.10.10.", "A popular long-form, stick-figure-illustrated blog about almost everything.");
+
+        PostAttributes engineer = new PostAttributes("One Engineer Is Trying to Get IBM to Reckon With Trump", "William Turton", "2017.03.28.", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.");
+    }
 }
+
--- week-05/src/day1ObjectOriented/preClassTask/day1ObjectOriented/blogPost/PostAttributes.java
+++ week-05/src/day1ObjectOriented/preClassTask/day1ObjectOriented/blogPost/PostAttributes.java
@@ -1,4 +1,17 @@
 package day1ObjectOriented.preClassTask.day1ObjectOriented.blogPost;
 
 public class PostAttributes {
+
+    String authorName;
+    String title;
+    String text;
+    String publicationDate;
+
+    public PostAttributes(String title, String authorName, String publicationDate, String text) {
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+    }
 }
+
