fab33e26ae79b89c540cffa122a58c4a0d665ca6
lilla <lilla@quince.hu>
2018-05-02 20:34:33 +0200

    postIt

---
+++ week-05/src/day1ObjectOriented/preClassTask/postIt/Ideas.java
@@ -0,0 +1,14 @@
+package day1ObjectOriented.preClassTask.postIt;
+
+import day1ObjectOriented.postIt.PostIt;
+
+public class Ideas {
+
+    public static void main(String[] args) {
+
+        PostIt idea1 = new PostIt("orange", "Idea 1", "blue");
+        PostIt idea2 = new PostIt("pink", "Awesome", "black");
+        PostIt idea3 = new PostIt("yellow", "Superb!", "green");
+
+    }
+}
