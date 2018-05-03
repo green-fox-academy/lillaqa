a3a275a88da0576b4654884190ef7d7511df2dc1
lilla <lilla@quince.hu>
2018-05-03 19:16:36 +0200

    add inputmismatch exceptions

--- week-04/src/day2FilesExceptions/DivideByZero.java
+++ week-04/src/day2FilesExceptions/DivideByZero.java
@@ -9,14 +9,17 @@ public class DivideByZero {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("gimme a number please");
-        int divideBy = scanner.nextInt();
-        scanner.close();
+
 
         try {
+            int divideBy = scanner.nextInt();
+            scanner.close();
             int result = 10 / divideBy;
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("Integers can't be divided by 0");
+        } catch (InputMismatchException e) {
+            System.out.println("String can't be divided, please add a number");
         }
 
     }
