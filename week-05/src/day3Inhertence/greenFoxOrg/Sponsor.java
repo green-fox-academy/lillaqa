e95fce878df2fd164cddb47deae45bdef716976f
lilla <lilla@quince.hu>
2018-05-12 17:06:34 +0200

    Green fox organisation -finish sponsor (WIP)

--- week-05/src/day3Inhertence/greenFoxOrg/Sponsor.java
+++ week-05/src/day3Inhertence/greenFoxOrg/Sponsor.java
@@ -6,20 +6,31 @@ public class Sponsor extends Person {
     int hiredStudents;
 
     public Sponsor() {
-
+        super("Jane Doe", 30, "female");
+        this.company = "Google";
+        this.hiredStudents = 0;
     }
+
+    public Sponsor(String name, int age, String gender, String company) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.company = company;
+        this.hiredStudents = 0;
+    }
+
+    public void introduce() {
+        super.introduce();
+        System.out.println(" who represents " + company + " and hired " + hiredStudents + " students so far.");
+    }
+
+    public void hire() {
+        hiredStudents++;
+    }
+
+    public void getGoal() {
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
 }
 
-
-/*
-Create a Sponsor class that has the same fields and methods as the Person class, and has the following additional
-
-
-method:
-introduce(): "Hi, I'm name, a age year old gender who represents company and hired hiredStudents students so far."
-hire(): increase hiredStudents by 1
-getGoal(): prints out "Hire brilliant junior software developers."
-The Sponsor class has the following constructors:
-
-Sponsor(name, age, gender, company): beside the given parameters, it sets hiredStudents to 0
-Sponsor(): sets name to Jane Doe, age to 30, gender to female, company to Google and hiredStudents to 0*/
