0428e98592f2246accd013ed0d055a1db92859c7
lilla <lilla@quince.hu>
2018-05-12 17:43:01 +0200

    Green fox organisation

--- week-05/src/day3Inhertence/greenFoxOrg/Cohort.java
+++ week-05/src/day3Inhertence/greenFoxOrg/Cohort.java
@@ -1,4 +1,32 @@
 package day3Inhertence.greenFoxOrg;
 
+
+import java.util.ArrayList;
+
 public class Cohort {
+
+    String name;
+    ArrayList<Student> students;
+    ArrayList<Mentor> mentors;
+
+    public Cohort(String name) {
+        this.name = name;
+        this.students = new ArrayList<>();
+        this.mentors = new ArrayList<>();
+    }
+
+    public ArrayList<Student> addStudent(Student student) {
+        students.add(student);
+        return students;
+    }
+
+    public ArrayList<Mentor> addMentor(Mentor mentor) {
+        mentors.add(mentor);
+        return mentors;
+    }
+
+    public void info() {
+        System.out.println("The " + name + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
+    }
+
 }
--- week-05/src/day3Inhertence/greenFoxOrg/Main.java
+++ week-05/src/day3Inhertence/greenFoxOrg/Main.java
@@ -1,20 +1,51 @@
 package day3Inhertence.greenFoxOrg;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Person lilla = new Person("Lilla", 32, "female");
+        ArrayList<Person> people = new ArrayList<>();
 
-        lilla.introduce();
+        Person mark = new Person("Mark", 46, "male");
+        people.add(mark);
+        Person jane = new Person();
+        people.add(jane);
+        Student john = new Student("John Doe", 20, "male", "BME");
+        people.add(john);
+        Student student = new Student();
+        people.add(student);
+        Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
+        people.add(gandhi);
+        Mentor mentor = new Mentor();
+        people.add(mentor);
+        Sponsor sponsor = new Sponsor();
+        people.add(sponsor);
+        Sponsor elon = new Sponsor("Elon Musk", 46, "male", "SpaceX");
+        people.add(elon);
 
-        Student rego = new Student("Regő", 5, "male", "Waldorf Ovi");
-        rego.introduce();
+        student.skipDays(3);
 
-        Mentor testingLady = new Mentor();
-        testingLady.introduce();
+        for (int i = 0; i < 5; i++) {
+            elon.hire();
+        }
 
-        Student smileFace = new Student();
-        smileFace.introduce();
+        for (int i = 0; i < 3; i++) {
+            sponsor.hire();
+        }
+
+        for(Person person : people) {
+            person.introduce();
+            person.getGoal();
+        }
+
+        Cohort awesome = new Cohort("AWESOME");
+        awesome.addStudent(student);
+        awesome.addStudent(john);
+        awesome.addMentor(mentor);
+        awesome.addMentor(gandhi);
+        awesome.info();
+
     }
 }
