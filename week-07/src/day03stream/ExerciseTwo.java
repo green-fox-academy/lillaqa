2b33fcc8434a94b8674c6e4c60a154ef5577bf09
lilla <lilla@quince.hu>
2018-05-18 20:21:51 +0200

    second exercise

---
+++ week-07/src/day03stream/ExerciseTwo.java
@@ -0,0 +1,18 @@
+package day03stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ExerciseTwo {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+        Double oddAverage = numbers.stream().filter(n -> n % 2 != 0).mapToDouble(n -> n).average().getAsDouble();
+        System.out.println(oddAverage);
+
+
+    }
+}
