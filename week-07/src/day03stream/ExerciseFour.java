826cf60aa82160d5825a9952fc2184a36246a0ba
lilla <lilla@quince.hu>
2018-05-19 17:42:57 +0200

    exercise 4

---
+++ week-07/src/day03stream/ExerciseFour.java
@@ -0,0 +1,19 @@
+package day03stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExerciseFour {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+
+        numbers.stream().map(n -> n*n).filter(n -> n > 20).forEach(System.out::println);
+
+    }
+}
+
+
+
+
+
