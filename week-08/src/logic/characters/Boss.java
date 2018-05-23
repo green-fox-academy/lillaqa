255faa2fd887ad2c7d7523911973bd78a275355f
lilla <lilla@quince.hu>
2018-05-23 19:11:14 +0200

    temporary random generated level for monsters

--- week-08/src/logic/characters/Boss.java
+++ week-08/src/logic/characters/Boss.java
@@ -4,7 +4,7 @@ public class Boss extends Characters {
     // TODO the level system needs to be re-written
 
     public Boss() {
-        this.level = 1;
+        this.level = randomD6(1, 3);
         this.maxHp = 2 * level * randomD6(1, 6) + randomD6(1, 6);
         this.dp = level / 2 * randomD6(1, 6) + randomD6(1, 6) /2;
         this.sp = level * randomD6(1, 6) + level;
--- week-08/src/logic/characters/Monster.java
+++ week-08/src/logic/characters/Monster.java
@@ -4,7 +4,7 @@ public class Monster extends Characters {
     // TODO the level system needs to be re-written
 
     public Monster() {
-        this.level = 1;
+        this.level = randomD6(1, 3);
         this.maxHp = 2 * level * randomD6(1, 6);
         this.dp = level / 2 * randomD6(1, 6);
         this.sp = level * randomD6(1, 6);
