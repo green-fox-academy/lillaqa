52a24996545f524751b51b8145c063ca010a930c
lilla <lilla@quince.hu>
2018-05-27 17:31:38 +0200

    character creation first version

---
+++ week-08/src/logic/logic/AddCharacters.java
@@ -0,0 +1,32 @@
+package logic.logic;
+
+import logic.characters.Boss;
+import logic.characters.Characters;
+import logic.characters.Hero;
+import logic.characters.Monster;
+
+import java.util.ArrayList;
+
+import static logic.characters.Characters.randomD6;
+
+public class AddCharacters {
+
+    public ArrayList<Characters> loadCharacters = new ArrayList<>();
+    int monsterNumber = randomD6(3, 6);
+
+    public void createCharacters() {
+        Hero hero = new Hero();
+        loadCharacters.add(hero);
+        Boss boss = new Boss();
+        loadCharacters.add(boss);
+
+    }
+
+    public void createMonster(int monsterNumber) {
+        for (int i = 0; i < monsterNumber; i++) {
+            Monster newMonster = new Monster();
+            loadCharacters.add(newMonster);
+        }
+
+    }
+}
