255d5926f4e072bcf078bbcc79530546641d7672
lilla <lilla@quince.hu>
2018-06-01 20:39:21 +0200

    exapmle from class a bit modified

---
+++ week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/Controller.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.springstart;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RestController
+public class Controller {
+    private String name;
+
+    @RequestMapping(method = RequestMethod.GET, value="/greetings")
+    //azért emelte ki márk a getet, mert a request mindenre működik
+    public ArrayList<String> getHellos() {
+        return new ArrayList<>(
+                Arrays.asList(
+                        "bonjour",
+                        "holá",
+                        "szia",
+                        "hello"
+                )
+        );
+    }
+
+}
