2bc12b2d68f9a93efe3198d42ff762644a224924
lilla <lilla@quince.hu>
2018-06-03 21:42:22 +0200

    helloRestController url typo fix

--- week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
-    @RequestMapping(value = "/greetings")
+    @RequestMapping(value = "/greeting")
     public Greetings greetings() {
 
         return new Greetings(1, "Hello világ!");
