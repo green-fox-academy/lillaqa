296e01e0fd87b327fb3f27ab8a7f536382b75280
lilla <lilla@quince.hu>
2018-06-03 21:45:57 +0200

    helloUserRESTApp

---
+++ week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloUserRESTApp.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloUserRESTApp {
+
+    @RequestMapping(value = "/greetings")
+    public Greetings greetingUser(@RequestParam("name") String name) {
+        return new Greetings(2, "Szia " + name);
+    }
+}
