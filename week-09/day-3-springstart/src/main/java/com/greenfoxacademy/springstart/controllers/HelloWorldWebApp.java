eccaf51cc56193824b80c351263bc5b12f66d15d
lilla <lilla@quince.hu>
2018-06-11 20:36:33 +0200

    Web Greet counter app

--- week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldWebApp.java
+++ week-09/day-3-springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldWebApp.java
@@ -3,14 +3,24 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWorldWebApp {
 
+    static AtomicLong countCalls = new AtomicLong();
+
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(@RequestParam (name = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("countCalls", countCalls.incrementAndGet());
         return "greeting";
     }
 }
+
+/*
+A%20kurva%20anyádba%20menjél%20el%20légyszi!
+ */
