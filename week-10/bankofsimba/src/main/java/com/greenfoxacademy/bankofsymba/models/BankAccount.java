44e2eae1d4ad27fd4242210fec3098538f50e44f
lilla <lilla@quince.hu>
2018-06-15 20:04:32 +0200

    bankaccount model class

--- week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsymba/models/BankAccount.java
+++ week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsymba/models/BankAccount.java
@@ -2,4 +2,19 @@ package com.greenfoxacademy.bankofsymba.models;
 
 public class BankAccount {
 
+    private String name;
+    private int balance;
+    private String animalType;
+
+    public BankAccount() {
+        this.name = "Simba";
+        this.balance = 2000;
+        this.animalType = "Lion";
+    }
+
+    public BankAccount(String name, int balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
 }
