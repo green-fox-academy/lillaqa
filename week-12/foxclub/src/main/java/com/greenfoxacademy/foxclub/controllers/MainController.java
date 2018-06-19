803db6a3d0da670e1ad60a868c501e64f44e02e8
lilla <lilla@quince.hu>
2018-06-20 00:27:46 +0200

    introduction part - image with url solution

--- week-12/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-12/foxclub/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,12 +1,13 @@
 package com.greenfoxacademy.foxclub.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "")
     public String index() {
         return "index";
     }
