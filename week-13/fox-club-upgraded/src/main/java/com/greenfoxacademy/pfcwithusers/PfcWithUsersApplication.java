e1784bd8f235e5ab9fdee54ab14283740e035d8c
lilla <lilla@quince.hu>
2018-06-27 18:42:37 +0200

    project init commit

---
+++ week-13/fox-club-upgraded/src/main/java/com/greenfoxacademy/pfcwithusers/PfcWithUsersApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.pfcwithusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcWithUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfcWithUsersApplication.class, args);
+    }
+}
