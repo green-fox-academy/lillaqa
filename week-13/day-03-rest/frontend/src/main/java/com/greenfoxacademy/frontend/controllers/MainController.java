b86e7a2784f8e219baafb812790e90f7892a70a2
lilla <lilla@quince.hu>
2018-07-01 22:30:30 +0200

    doubling function ready

--- week-13/day-03-rest/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/MainController.java
+++ week-13/day-03-rest/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/MainController.java
@@ -1,15 +1,21 @@
 package com.greenfoxacademy.frontend.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class MainController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @GetMapping("")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("doubling")
+    public int doubling(@RequestParam(value = "input", required= false) Integer input) {
+        Integer result = input * 2;
+        return result;
     }
 
 }
