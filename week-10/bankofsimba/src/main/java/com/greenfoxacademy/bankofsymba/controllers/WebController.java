6df780a1787b10e66f18ce67d6e67559f6a19843
lilla <lilla@quince.hu>
2018-07-22 21:04:21 +0200

    make it controller (wip, empty)

--- week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsymba/controllers/WebController.java
+++ week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsymba/controllers/WebController.java
@@ -9,13 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebController {
 
-    @RequestMapping(value = "/show")
-    public BankAccount show() {
-        return new BankAccount();
-    }
+
 
 
 }
